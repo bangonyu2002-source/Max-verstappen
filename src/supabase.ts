184329c71b5bdab7549f57e65087bce71da80571
bangonyu <bangonyu2002@example.com>
2025-12-19 14:29:39 +0900

    Update correct supabase anon key

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://cbbuadleqklcikdvbmva.supabase.co'; 
-const supabaseKey = 'sb_publishable_CGqZe_UpVDtRRAD9t0qbNw_QrT5r8hv'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiYnVhZGxlcWtsY2lrZHZibXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MDk4MjgsImV4cCI6MjA4MTI4NTgyOH0.936Quv2A4IUc-F_gWygk0Z1nm4DBygL3m8FSXLCD--Q'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
